5992d55a92481d6f61afb2fbffaf85ba357cc26e
Ashlyn Knox <ashlyn@lilyx.io>
2024-07-29 12:29:33 -0600

    example solution code

---
+++ exampleSolution.js
@@ -0,0 +1,13 @@
+console.log("Syntax Error: The code is missing a variable declaration");
+
+/* 
+Broken Code:
+const a = 5
+console.log(a + b)
+*/
+
+// Correct Code
+const a = 5;
+const b = 25;
+
+console.log(a + b);
